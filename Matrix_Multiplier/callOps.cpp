ce67431ab790e0d468c7148b7e05ceb2592547ee
izzy1721 <50534764+izzy1721@users.noreply.github.com>
2019-05-27 12:31:27 -0500

    Update callOps.cpp

--- Matrix_Multiplier/callOps.cpp
+++ Matrix_Multiplier/callOps.cpp
@@ -115,3 +115,60 @@ void doAddition()
     delete sum;
 
 } //end of doAddition()
+
+void doMultiply() {
+    std::cout << "Please enter a size for first matrix in form \"x y\":\n";
+    int xSize = -1;
+    int ySize = -1;
+    std::cin >> xSize;
+    std::cin >> ySize;
+
+    Matrix *A = new Matrix(xSize, ySize);
+    int maxElems = A->getMaxElem();
+    int count = 0;
+    int input = 0;
+    std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
+    while (count != maxElems)
+    {
+        std::cin >> input;
+        A->push(input);
+        count++;
+    }
+    std::cout << "Please enter a size for second matrix in form \"x y\":\n";
+    xSize = -1;
+    ySize = -1;
+    std::cin >> xSize;
+    std::cin >> ySize;
+
+    Matrix *B = new Matrix(xSize, ySize);
+    maxElems = B->getMaxElem();
+    count = 0;
+    input = 0;
+    std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
+    while (count != maxElems)
+    {
+        std::cin >> input;
+        B->push(input);
+        count++;
+    }
+    std::cout << "Matrix A:\n";
+    A->printArr();
+    std::cout << "Matrix B:\n";
+    B->printArr();
+
+    Matrix *product;
+
+    product = product->multiplication(A, B); 
+
+    if (product == nullptr)
+    {
+        std::cout << "Error in computation\n";
+        return;
+    }
+
+    delete A;
+    delete B;
+    std::cout << "Resulting Matrix:\n";
+    product->printArr();
+    delete product;
+} //end of doMultiply()
