164a0c00960fdf82d85abe0d7cbabe6613262a51
izzy1721 <50534764+izzy1721@users.noreply.github.com>
2019-05-27 11:33:29 -0500

    Update library.h

--- Matrix_Multiplier/library.h
+++ Matrix_Multiplier/library.h
@@ -1,3 +1,9 @@
+/*
+The purpose of this header is to provide foward definitions of all callOps.cpp functions to be used in main.cpp.
+Include all libraries used within this heder file for ease of use elsewhere. 
+*/
+
+
 #include <iostream>
 #include <string.h>
 #include "Matrix.h"
