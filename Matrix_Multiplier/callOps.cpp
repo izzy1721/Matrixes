81cae7c7dbf13237f4e714cf5b53dd2e5de82902
izzy1721 <50534764+izzy1721@users.noreply.github.com>
2019-05-28 07:22:04 -0500

    Update callOps.cpp

--- Matrix_Multiplier/callOps.cpp
+++ Matrix_Multiplier/callOps.cpp
@@ -2,14 +2,18 @@
 
 void doSubtraction()
 {
+    //enter a size of a matrix to be created
     std::cout << "Please enter a size for first matrix in form \"x y\":\n";
     int xSize = -1;
     int ySize = -1;
     std::cin >> xSize;
     std::cin >> ySize;
 
+    //create new matrix and get the max elements that it can have
     Matrix *A = new Matrix(xSize, ySize);
     int maxElems = A->getMaxElem();
+
+    //input the number of elements into the matrix
     int count = 0;
     int input = 0;
     std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
@@ -19,14 +23,19 @@ void doSubtraction()
         A->push(input);
         count++;
     }
+
+    //enter a size of a matrix to be created
     std::cout << "Please enter a size for second matrix in form \"x y\":\n";
     xSize = -1;
     ySize = -1;
     std::cin >> xSize;
     std::cin >> ySize;
 
+    //create new matrix and get the max elements that it can have
     Matrix *B = new Matrix(xSize, ySize);
     maxElems = B->getMaxElem();
+
+    //input the number of elements into the matrix
     count = 0;
     input = 0;
     std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
@@ -36,22 +45,30 @@ void doSubtraction()
         B->push(input);
         count++;
     }
+
+    //print out neewly created matricies
     std::cout << "Matrix A:\n";
     A->printArr();
     std::cout << "Matrix B:\n";
     B->printArr();
 
+    //create pointer to a matrix
     Matrix *subtracted;
+    //function subtraction() retunrs a pointer to a newly created matrix so point
+    //matrix pointer created above to the result of the function
     subtracted = subtracted->subtraction(A, B);
 
+    //A and B are no longer being used so delete them
+    delete A;
+    delete B;
+
+    //if the function returned a nullptr can't do anything after this point so return
     if (subtracted == nullptr)
     {
         std::cout << "Error in computation\n";
         return;
     }
-
-    delete A;
-    delete B;
+    //if not a nullptr then print out the reuslting matrix and then delete it
     std::cout << "Resulting Matrix:\n";
     subtracted->printArr();
     delete subtracted;
@@ -60,14 +77,18 @@ void doSubtraction()
 
 void doAddition()
 {
+    //enter a size of a matrix to be created
     std::cout << "Please enter a size for first matrix in form \"x y\":\n";
     int xSize = -1;
     int ySize = -1;
     std::cin >> xSize;
     std::cin >> ySize;
 
+    //create new matrix and get the max elements that it can have
     Matrix *A = new Matrix(xSize, ySize);
     int maxElems = A->getMaxElem();
+
+    //input the number of elements into the matrix
     int count = 0;
     int input = 0;
     std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
@@ -77,14 +98,19 @@ void doAddition()
         A->push(input);
         count++;
     }
+
+    //enter a size of a matrix to be created
     std::cout << "Please enter a size for second matrix in form \"x y\":\n";
     xSize = -1;
     ySize = -1;
     std::cin >> xSize;
     std::cin >> ySize;
 
+    //create new matrix and get the max elements that it can have
     Matrix *B = new Matrix(xSize, ySize);
     maxElems = B->getMaxElem();
+
+    //input the number of elements into the matrix
     count = 0;
     input = 0;
     std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
@@ -94,22 +120,30 @@ void doAddition()
         B->push(input);
         count++;
     }
+
+    //print out neewly created matricies
     std::cout << "Matrix A:\n";
     A->printArr();
     std::cout << "Matrix B:\n";
     B->printArr();
 
+    //create pointer to a matrix
     Matrix *sum;
+    //function addition() retunrs a pointer to a newly created matrix so point
+    //matrix pointer created above to the result of the function
     sum = sum->addition(A, B);
 
+    //A and B are no longer being used so delete them
+    delete A;
+    delete B;
+
+    //if the function returned a nullptr can't do anything after this point so return
     if (sum == nullptr)
     {
         std::cout << "Error in computation\n";
         return;
     }
-
-    delete A;
-    delete B;
+    //if not a nullptr then print out the reuslting matrix and then delete it
     std::cout << "Resulting Matrix:\n";
     sum->printArr();
     delete sum;
@@ -118,14 +152,18 @@ void doAddition()
 
 void doMultiply()
 {
+    //enter a size of a matrix to be created
     std::cout << "Please enter a size for first matrix in form \"x y\":\n";
     int xSize = -1;
     int ySize = -1;
     std::cin >> xSize;
     std::cin >> ySize;
 
+    //create new matrix and get the max elements that it can have
     Matrix *A = new Matrix(xSize, ySize);
     int maxElems = A->getMaxElem();
+
+    //input the number of elements into the matrix
     int count = 0;
     int input = 0;
     std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
@@ -135,14 +173,19 @@ void doMultiply()
         A->push(input);
         count++;
     }
+
+    //enter a size of a matrix to be created
     std::cout << "Please enter a size for second matrix in form \"x y\":\n";
     xSize = -1;
     ySize = -1;
     std::cin >> xSize;
     std::cin >> ySize;
 
+    //create new matrix and get the max elements that it can have
     Matrix *B = new Matrix(xSize, ySize);
     maxElems = B->getMaxElem();
+
+    //input the number of elements into the matrix
     count = 0;
     input = 0;
     std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
@@ -152,23 +195,30 @@ void doMultiply()
         B->push(input);
         count++;
     }
+
+    //print out neewly created matricies
     std::cout << "Matrix A:\n";
     A->printArr();
     std::cout << "Matrix B:\n";
     B->printArr();
 
+    //create pointer to a matrix
     Matrix *product;
-
+    //function multiplication() retunrs a pointer to a newly created matrix so point
+    //matrix pointer created above to the result of the function
     product = product->multiplication(A, B);
 
+    //A and B are no longer being used so delete them
+    delete A;
+    delete B;
+
+    //if the function returned a nullptr can't do anything after this point so return
     if (product == nullptr)
     {
         std::cout << "Error in computation\n";
         return;
     }
-
-    delete A;
-    delete B;
+    //if not a nullptr then print out the reuslting matrix and then delete it
     std::cout << "Resulting Matrix:\n";
     product->printArr();
     delete product;
@@ -176,14 +226,18 @@ void doMultiply()
 
 void doGauss()
 {
+    //enter a size of a matrix to be created
     std::cout << "Please enter a size for matrix in form \"x y\":\n";
     int xSize = -1;
     int ySize = -1;
     std::cin >> xSize;
     std::cin >> ySize;
 
+    //create new matrix and get the max elements that it can have
     Matrix *A = new Matrix(xSize, ySize);
     int maxElems = A->getMaxElem();
+
+    //input the number of elements into the matrix
     int count = 0;
     int input = 0;
     std::cout << "Please enter " << maxElems << " numbers to\ninsert into the matrix:\n";
@@ -193,17 +247,28 @@ void doGauss()
         A->push(input);
         count++;
     }
+
+    //print out neewly created matrix
+    std::cout << "Matrix A:\n";
     A->printArr();
 
+    //create pointer to a matrix
     Matrix *solved;
+    //function gauss() retunrs a pointer to a newly created matrix so point
+    //matrix pointer created above to the result of the function
     solved = solved->gauss(A);
+
+    //A is no longer being used so delete it
+    delete A;
+
+    //if the function returned a nullptr can't do anything after this point so return
     if (solved == nullptr)
     {
         std::cout << "Error in computation\n";
         return;
     }
-
-    delete A;
+    //if not a nullptr then print out the reuslting matrix and then delete it
+    std::cout << "Resulting Matrix:\n";
     solved->printArr();
     delete solved;
 } //end of doGauss()
